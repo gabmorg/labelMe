bb12dbabe792a6d7924da9082d8aed6eaecd0f7f
gabmorg <gab.morgenshtern@gmail.com>
2019-10-05 00:11:34 -0400

    comment out the broken tests! Sorry 12:10

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,26 +1,6 @@
 library(shinytest)
 library(testthat)
-
-# The following code for testing Shiny app was borrowed from a Shinytest
-# tutorial by Ferand Dalatieh
-# (Unit) Testing Shiny apps using testthat
-# (https://www.r-bloggers.com/unit-testing-shiny-apps-using-testthat/)
-
-context("Shiny app test")
-
-# open Shiny app and PhantomJS
-# app <- ShinyDriver$new("")
+library(labelMe)
 
 
-# The code below to setup the testing process comes from Dr. Steipe's rpt package
-# (https://github.com/hyginn/rpt):
-
-# parse the package name from the parent directory path (necessary because
-# normal tests and R CMD Check tests are run from different directories)
-# x <- unlist(strsplit(gsub("(\\.Rcheck/tests)?$", "", getwd()), "/"))
-# pkgName <- x[length(x)]
-#
-# library(pkgName, character.only = TRUE)
-#
-# test_check(pkgName)
-# [END]
+test_check("labelMe")
---
+++ tests/testthat/helper-test-serveMe.R
@@ -0,0 +1,1 @@
+LABELS <- list("test label1", "test label2", "unknown")
