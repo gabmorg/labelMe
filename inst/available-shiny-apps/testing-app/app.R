c583a4af86166b06de754c5eb0fff9854a7d6543
gabmorg <gab.morgenshtern@gmail.com>
2019-10-31 17:49:54 -0400

    - pagination resets on "flush data" button click - small but w the imgName rendered text not resetting - download functionality implemented!

--- inst/available-shiny-apps/testing-app/app.R
+++ inst/available-shiny-apps/testing-app/app.R
@@ -33,20 +33,16 @@ ui <- fluidPage(
                 else if(key === 37) {
                   Shiny.onInputChange("leftArrow", [e.which, e.timeStamp]);
                 }
-
               });
               '),
   sidebarLayout(
     position = "right",
     sidebarPanel(
       h3("Images"),
-      fileInput(inputId = 'files',
-                label = 'Upload labeling images here',
-                multiple = TRUE,
-                accept=c('image/png', 'image/jpeg', 'image/jpg', 'image/pdf', 'image/dicom-rle', 'image/jls')),
+      uiOutput('fileInputs'),
       fluidRow(
-        actionButton("saveLabels", "Save labels"),
-        downloadButton('download',"Download labels.csv")
+        actionButton("flushData", "flush data"),
+        downloadButton("download","Download DATE-labels.csv")
       )
     ),
     mainPanel(
@@ -63,25 +59,61 @@ ui <- fluidPage(
 
 server <- shinyServer(function(input, output) {
   imagePages <- reactiveValues(page = 1)
-  output$fileTable <- renderTable(input$files)
 
   # pagination navigation helper:
-  navigate <- function(direction) {
-    imagePages$page <- imagePages$page + direction
+  navigate <- function(direction, toStart = FALSE) {
+    if(toStart) {
+      imagePages$page <- 1
+    }
+    else {
+      imagePages$page <- imagePages$page + direction
+    }
+
   }
 
-  # access via files()
+  # access with files()
   files <- reactive({
+    input$flushData
     files <- input$files
     files$image_key <- c(1:nrow(input$files))
     files$datapath <- gsub("\\\\", "/", files$datapath)
-    files$label <- selectedOption()
+    files$label <- c("Unknown")
     files
   })
 
+  # nrowUploaded <- reactiveValues(nrows = length(files()$name))
+  # To be built dynamically upon each labeling event
+  selectiondf <- reactiveValues()
+  selectiondf$df <- data.frame(
+    # image_name = files$name,
+    # image_key = c(1:nrowUploaded$nrows),
+    image_key = c(1),
+    image_name = c("test image_name"),
+    label_1 = c("Unknown"),
+    label_2 = c("Not applicable"),
+    stringsAsFactors = FALSE
+  )
+
+  # Display uploaded file info
+  output$fileTable <- renderTable({
+    input$flushData
+    if(is.null(input$files)) return(NULL)
+    else{
+      return(files())
+    }
+  })
+
+  output$fileInputs <- renderUI({
+    input$flushData
+    fileInput(inputId = 'files',
+            label = 'Upload labeling images here',
+            multiple = TRUE,
+            accept=c('image/png', 'image/jpeg', 'image/jpg', 'image/pdf', 'image/dicom-rle', 'image/jls'))
+    })
 
   # paginated images setup for uiOutput('images')
   output$images <- renderUI({
+    input$flushData
     if(is.null(input$files)) return(NULL)
     else {
       imageId <- paste0("image", imagePages$page)
@@ -90,43 +122,65 @@ server <- shinyServer(function(input, output) {
   })
 
   # paginated radio button setup for uiOutput('radios')
-  output$imgName <- renderText({files()$name[imagePages$page]})
+  output$imgName <- renderText({
+    files()$name[imagePages$page]
+  })
   output$radios <- renderUI({
+    input$flushData
     if(is.null(input$files)) return(NULL)
     else {
-    radioId <- paste0("radio", imagePages$page)
-    radioButtons(inputId = radioId,
-                 label = textOutput("imgName"),
-                 choices = c("Saggital", "Transverse", "Unknown"), selected = "Unknown")
+      radioIdA <- paste0("radioA", imagePages$page)
+      radioIdB <- paste0("radioB", imagePages$page)
+      fluidRow(
+        column(5, offset = 1,radioButtons(inputId = radioIdA,
+                                          label = textOutput("imgName"),
+                                          choices = c("Saggital", "Transverse", "Bladder", "Unknown"),
+                                          selected = "Unknown")),
+        column(5, offset = 1,radioButtons(inputId = radioIdB,
+                                          label = "View type",
+                                          choices = c("Left", "Right", "Not Applicable"),
+                                          selected = "Not Applicable"))
+        )
     }
   })
 
   # visual confirmation of selected radio button option (default is Unknown)
+  # this code also attaches the label to the selection dataframe, which
+  # is updated whenever a radio button option is selected at the row corresponding
+  # to the image page
   selectedOption <- reactive({
     if(is.null(input$files)) {
       return("NULL")
     }
     else {
-      radioId <- paste0("radio", imagePages$page)
-      print(input$radioId)
-      input$radioId
+      radioIdA <- paste0("radioA", imagePages$page)
+      radioIdB <- paste0("radioB", imagePages$page)
+      selectiondf$df[imagePages$page, "image_key"] <- imagePages$page
+      selectiondf$df[imagePages$page, "image_name"] <- files()$name[imagePages$page]
+      selectiondf$df[imagePages$page, "label_1"] <- input[[radioIdA]]
+      selectiondf$df[imagePages$page, "label_2"] <- input[[radioIdB]]
+
+      # Debug print:
+      print(head(selectiondf$df))
+      print("testing reactive obj, line 155, after labeling:")
+      print(head(selectiondf$df))
+      print(paste0("Image label: ", input[[radioIdA]], input[[radioIdB]]))
+      return(c(input[[radioIdA]],input[[radioIdB]]) )
     }
   })
-
-
-  output$radioSelection <- renderText({paste0("Image label: ", selectedOption())})
+  output$radioSelection <- renderText({paste0(selectedOption(), ",")})
 
   # Paginated image rendering - From SO post: [LINK]
-  observe({
+  observeEvent(input$files, {
     if(is.null(input$files)) return(NULL)
-    for (i in 1:nrow(files())) {
+    for (i in 0:nrow(files())) {
       local({
-        my_i <- i
-        imageIndex <- paste0("image", my_i)
+        local_i <- i
+        imageIndex <- paste0("image", local_i)
         output[[imageIndex]] <-
           renderImage({
             list(
-              src = files()$datapath[my_i],
+              src = files()$datapath[local_i],
               width = 400,
               height = 400,
               alt = "Image upload failed!"
@@ -145,7 +199,7 @@ server <- shinyServer(function(input, output) {
     else navigate(0)
 
     # TO DELETE: bug tracking
-    # print(imagePages$page)
+    print(imagePages$page)
   })
 
   observeEvent(input$leftArrow,{
@@ -155,7 +209,7 @@ server <- shinyServer(function(input, output) {
     else navigate(0)
 
     # TO DELETE: bug tracking
-    # print(imagePages$page)
+    print(imagePages$page)
   })
 
   # Download a file with the name labels-DATE.csv
@@ -166,12 +220,33 @@ server <- shinyServer(function(input, output) {
       paste(Sys.Date(),"-labels", ".csv", sep="")
     },
     content = function(fname){
-      write.csv(files(), fname)
+      write.csv(selectiondf$df, fname)
     },
     contentType = "text/csv"
   )
 
+  observeEvent(input$flushData, {
+    for (i in 0:nrow(files())) {
+      local({
+        local_i <- i
+        imageIndex <- paste0("image", local_i)
+        output[[imageIndex]] <- NULL
+      })
+    }
+
+    selectiondf$df <- data.frame(
+      image_key = c(1),
+      image_name = c("test image_name"),
+      label_1 = c("Unknown"),
+      label_2 = c("Not applicable"),
+      stringsAsFactors = FALSE
+    )
+
+    files <- NULL
+    navigate(0, toStart = TRUE)
+  })
 })
+
 
 
 shinyApp(ui=ui,server=server)
